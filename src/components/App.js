521213b3480149e02028fe8d127e86e0db3ef355
121ShoryaSingh <shoryasingh810@gmail.com>
2025-12-12 18:10:06 +0530

    first try 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [input, setInput] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  useEffect(()=>{
+    setSum( prevSum =>Number(prevSum) + Number(input))
+  },[input])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="number" onChange={(e)=>{setInput(e.target.value)}}/>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
